6f5d7d80fdb44932b6bc193ef0f63bc0cae42d5f
mj0144 <minjeong0144@gmail.com>
2022-04-19 23:03:55 +0900

    1. 애노테이션 추가. @Retry : 애노테이션으로 예외 발생 시 재시도 @Trace : 애노테이션으로 로그 출력

---
+++ src/main/java/hello/aop/RetryAspect.java
@@ -0,0 +1,34 @@
+package hello.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+    // 매개변수로 적은 어노테이션이 hello.aop.exam.annotation.Retry 대체될수있음.
+    @Around("@annotation(retry)")
+    public void doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry = {}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount =1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+
+            }
+        }
+        throw exceptionHolder;
+
+    }
+}
